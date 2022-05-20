fa844102074a4df524a74ec3dacb1993d3453d67
M4TT3O-91 <teo_exc@hotmail.it>
2022-05-20 09:52:00 +0200

    initial commit

---
+++ CRUDImpiegati/Controllers/ProvaController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+
+namespace CRUDImpiegati.Controllers
+{
+    public class ProvaController :Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
